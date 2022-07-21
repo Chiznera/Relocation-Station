67cfeeb78a581d9d2c76d6553c00c1d4289c5208
Chiznera <chiznera@gmail.com>
2022-07-21 22:26:26 +0000

    change to Navbar, Home page

---
+++ src/front/js/component/cardRow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CardRow = () => {
+  return (
+    <div className="container text-light bg-dark">
+      <div className="row">
+        <div className="hiddenScroll d-flex flex-row">City Cards Go Here</div>
+      </div>
+    </div>
+  );
+};
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,5 +5,19 @@ import "../../styles/home.css";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return <div></div>;
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
