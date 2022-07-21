793f1b306f5a65d7356044024fe5c093d3171873
haukatkinson <haukatkinson@aol.com>
2022-07-21 21:11:43 +0000

    Navbar and Jumbotron work

---
+++ src/front/js/component/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => (
+  <form action="/" method="get">
+    <label htmlFor="header-search">
+      <span className="visually-hidden">Search blog posts</span>
+    </label>
+    <input type="text" id="header-search" placeholder="Search" name="s" />
+    <button type="submit">Search</button>
+  </form>
+);
+
+export { SearchBar };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  $(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+      $("nav ul").toggleClass("showing");
+    });
+  });
+
+  // Scrolling Effect
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+      $("nav").addClass("black");
+    } else {
+      $("nav").removeClass("black");
+    }
+  });
+  return (
+    <div class="wrapper">
+      <header>
+        <nav>
+          <div class="menu-icon">
+            <i class="fa fa-bars fa-2x"></i>
+          </div>
+          <div class="logo">LOGO</div>
+          <div class="menu text-warning">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 };
