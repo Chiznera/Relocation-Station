36bb63e3831324a7e8647c3fd5f19f17849c6ea3
Chiznera <chiznera@gmail.com>
2022-07-23 00:33:33 +0000

    Created city details template page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -18,6 +18,116 @@ export const Home = () => {
           </div>
         </div>
       </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-5">
+        <h1 className="offset-2">Popular Cities</h1>
+
+        <div className="container text-light bg-dark">
+          <div className="row">
+            <div className="hiddenScroll d-flex flex-row">
+              City Cards Go Here
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
