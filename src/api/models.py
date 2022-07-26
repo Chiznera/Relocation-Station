b3eaaa72bbe542652d0c89f0ffd33b38fee1b137
Chiznera <chiznera@gmail.com>
2022-07-26 00:18:49 +0000

    adjusted user models

--- src/api/models.py
+++ src/api/models.py
@@ -11,8 +11,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -24,3 +24,14 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    temperature_range = db.Column(db.String(256), unique=False, nullable=False)
+    inclement_weather = db.Column(db.String(256), unique=False, nullable=False)
+    avg_cost_of_living = db.Column(db.String(256), unique=False, nullable=False)
+
+class Favorites(db.Model):
+    id = db.Column(db.String(256), unique=False, nullable=False)
