f37528fda8b61947e1cf2354926ddc755760750c
haukatkinson <haukatkinson@aol.com>
2022-08-11 01:02:44 +0000

    search bar complete

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Dark from "./darkMode";
 import logo from "./logo.png";
+
 
 export const Navbar = () => {
   return (
     <nav>
       <Link to={"/"}>
-        <img src={logo} class="logo w-25 p-3" />
+        <img src={logo} className="logo w-25 p-3" />
       </Link>
-      <div class="menu">
-        <div class="button-div">
+      <div className="menu">
+        <div className="button-div">
           <Link to={"/signup"}>
-            <button class="button-74">Signup</button>
+            <button className="button-74">Signup</button>
           </Link>
           <Link to={"/login"}>
-            <button class="button-84">Log In</button>
+            <button className="button-84">Log In</button>
           </Link>
         </div>
       </div>
